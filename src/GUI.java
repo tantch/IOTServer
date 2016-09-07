dc9f234abbad053523a2429921a9e634b1319e15
Leonardo faria <leonardofaria94@gmail.com>
2016-09-07 20:25:26 +0100

    This is my app. Ye beat it.

---
+++ src/GUI.java
@@ -0,0 +1,97 @@
+import java.awt.EventQueue;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class GUI {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 * 
+	 * @throws IOException
+	 */
+	public GUI() throws IOException {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 * 
+	 * @throws IOException
+	 */
+	private void initialize() throws IOException {
+		IOTServerThread thread = new IOTServerThread();
+		thread.start();
+		Object[][] data;
+
+		String[] columnNames = { "Private Network Name", "Number of Users", "Green State", "Red State" };
+
+		DefaultTableModel dtm = new DefaultTableModel(0, 0);
+		dtm.setColumnIdentifiers(columnNames);
+
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 342);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JScrollPane scrollPane = new JScrollPane();
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+		table = new JTable(dtm);
+		table.setModel(dtm);
+		scrollPane.setViewportView(table);
+
+		TableWatcher tw = new TableWatcher(dtm, thread);
+		tw.start();
+	}
+
+	public class TableWatcher extends Thread {
+		DefaultTableModel d;
+		IOTServerThread i;
+
+		public TableWatcher(DefaultTableModel d, IOTServerThread i) {
+			this.d = d;
+			this.i = i;
+		}
+
+		public void run() {
+			while (true) {
+				try {
+					sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				while (d.getRowCount() > 0)
+					d.removeRow(0);
+				for (String network : i.dudos.keySet()) {
+					d.addRow(new Object[] { network, i.dudos.get(network).peers.size(), i.dudos.get(network).redstate,
+							i.dudos.get(network).greenstate });
+				}
+
+			}
+		}
+	}
+
+}
--- src/IOTServerThread.java
+++ src/IOTServerThread.java
@@ -2,12 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class IOTServerThread extends Thread {
+
+	public class Network {
+		public int redstate;
+		public int greenstate;
+		public ArrayList<InetAddress> peers;
+
+		public Network(int state) {
+			this.redstate = state;
+			this.greenstate = 1;
+			peers = new ArrayList<>();
+		}
+	}
 
 	protected DatagramSocket socket = null;
 	protected BufferedReader in = null;
 	protected boolean cnt = true;
+
+	public ConcurrentHashMap<String, Network> dudos;
 
 	public IOTServerThread() throws IOException {
 		this("IOTServerThread");
@@ -17,28 +35,40 @@ public class IOTServerThread extends Thread {
 		super(name);
 		socket = new DatagramSocket(3333);
 
-		
 	}
 
 	public void run() {
 
-		
+		dudos = new ConcurrentHashMap<>();
 		System.out.println("running");
 		while (cnt) {
 			try {
-				System.out.println("1");
 				byte[] buf = new byte[256];
 
 				// receive request
 				DatagramPacket packet = new DatagramPacket(buf, buf.length);
 				socket.receive(packet);
-				System.out.println("2");
 				String rsp = new String(packet.getData(), 0, packet.getLength());
-				System.out.println("A:" + rsp);
-				
-				byte[] buf2 = ("This client status").getBytes();
-			      DatagramPacket out = new DatagramPacket(buf2, buf2.length, packet.getAddress(), packet.getPort());
-			      socket.send(out);
+				String[] message = rsp.split(":");
+				if (message.length != 2) {
+					System.out.println("Bad messagem, check your code.");
+					continue;
+				}
+				if (!dudos.containsKey(message[0]))
+					dudos.put(message[0], new Network(Integer.parseInt(message[1])));
+				else {
+					if (dudos.get(message[0]).redstate == 0)
+						dudos.get(message[0]).redstate = 1;
+					else
+						dudos.get(message[0]).redstate = 0;
+				}
+				if (!dudos.get(message[0]).peers.contains(packet.getAddress()))
+					dudos.get(message[0]).peers.add(packet.getAddress());
+				for (InetAddress peer : dudos.get(message[0]).peers) {
+					byte[] buf2 = ("" + dudos.get(message[0]).redstate).getBytes();
+					DatagramPacket out = new DatagramPacket(buf2, buf2.length, peer, 4000);
+					socket.send(out);
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 				cnt = false;
@@ -46,6 +76,5 @@ public class IOTServerThread extends Thread {
 		}
 		socket.close();
 	}
-
 
 }
